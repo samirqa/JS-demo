bd039926170e4cfce1f4d9b7aac292421dfa588e
Samir Patel <sameer21uk@gmail.com>
2023-06-29 22:46:55 +0100

    Add class

---
+++ jsclass.js
@@ -0,0 +1,11 @@
+class Employee{
+    setDetails(name, id){
+    this.name = name
+    this.id = id
+    }
+}
+
+let emp1 = new Employee();
+emp1.setDetails('Jhon',1001);
+console.log('Name of emp is :'+emp1.name)
+console.log('Id of emp is :'+emp1.id )
